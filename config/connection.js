95b4810358d0be07a1856244b453a6234372ace4
Bryan Isles <bryanisles@gmail.com>
2017-12-17 09:47:02 -0600

    Hooking this project with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: process.env.MYSQL_ROOT_PASSWORD,
-	database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);	
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "root",
+		password: process.env.MYSQL_ROOT_PASSWORD,
+		database: "burgers_db"
+	});
+};
 
 connection.connect(function(err) {
   if (err) {
